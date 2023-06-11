019fa6acf61c2e73dd897cf2dadc7be8f6bb0b5c
LanceLot <34064977+XcloudFance@users.noreply.github.com>
2023-06-11 14:56:36 +0800

    Revert "Chjy"

--- src/test/java/dsgp6/fakebook/UserRepository.java
+++ src/test/java/dsgp6/fakebook/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByUsername(String username);
+
+    User findByUid(String uid);
 }
